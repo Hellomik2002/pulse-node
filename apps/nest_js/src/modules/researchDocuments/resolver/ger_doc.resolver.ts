27664f3846be5d5bc57c9bdf862ff34c9874fdc9
hellomik <hellomk2002@gmail.com>
2023-03-07 18:13:07 +0600

    labs

---
+++ apps/nest_js/src/modules/researchDocuments/resolver/ger_doc.resolver.ts
@@ -0,0 +1,32 @@
+import { UseGuards } from "@nestjs/common";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { PrismaService } from "nestjs-prisma";
+import { ConsulationListCreateInput } from "src/@generated/consulation-list/consulation-list-create.input";
+import { ConsulationList } from "src/@generated/consulation-list/consulation-list.model";
+import { ResearchDocumentCreateInput } from "src/@generated/research-document/research-document-create.input";
+import { ResearchDocument } from "src/@generated/research-document/research-document.model";
+import { UserEntity } from "src/auth/tools/user.decorator";
+import { GqlAuthGuard } from "src/modules/auth/tools/gql-auth.guard";
+import { mainPrismaClient } from "src/prisma/main_client";
+
+import { PulseUser } from "@calcom/prisma_pulse";
+
+@Resolver()
+export class ResearchDocumentsResolver {
+  // inject prisma service here
+  private readonly prisma: PrismaService;
+
+  @Query(() => ResearchDocument)
+  @UseGuards(GqlAuthGuard)
+  async getMyResearchDocument(@UserEntity() { id: userId }: PulseUser): Promise<ResearchDocument> {
+    return await mainPrismaClient.researchDocument.findFirst({
+      where: { id: userId },
+      include: {
+        patient: true,
+        author: true,
+      },
+    });
+  }
+}
+
+// create ResearchDocumentsResolver here with same operations but different name
